b1c0efb18385b5270ed411aacbb70d8ea3ff58dd
aysegulgules <159700271+aysegulgules@users.noreply.github.com>
2024-08-08 10:42:45 +0300

    Add project files.

---
+++ PatikaWeek4Homework4/Car.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaWeek4Homework4
+{
+    public class Car
+    {
+        //Fields'lar tanımlanıyor.
+        private string? _brand;
+        private string? _model;
+        private string? _color;
+        private int numberDoors;
+
+        //Property'ler tanımlanıyor
+        public string Brand {
+            get
+            {
+                return _brand;
+            }
+            set
+            { 
+                _brand = value;
+            } 
+        }
+        public string Model { 
+            get
+            { 
+                return _model;
+            }
+            set
+            {
+                _model = value;
+            }
+        }
+
+        public string Color 
+        {
+            get
+            { 
+                return _color;            
+            }
+            set 
+            { 
+                _color = value;
+            }
+        }
+        public int NumberDoors 
+        {
+            get
+            {
+                return numberDoors;
+            }
+            set
+            {  //Kapı sayısının 2 veya 4 olması kontrol ediliyor.
+                if(value==2 ||value==4)
+                {
+                    numberDoors = value;
+                }
+                else
+                {
+                    Console.WriteLine("\nKapı sayısı 2 veya 4 girilmeliydi.");
+                    numberDoors = -1;
+
+                }
+            }     
+                
+        }
+ 
+        public void CarInfo()
+        {
+            Console.WriteLine($"\n Markası..:{Brand}\n Modeli..:{Model}\n Rengi..:{Color}\n Kapı Sayısı..:{NumberDoors}");
+        }
+
+    }
+}
---
+++ PatikaWeek4Homework4/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using PatikaWeek4Homework4;
+
+Console.WriteLine("---------Araba Bilgileri---------------");
+
+
+//Araba nesneleri oluşturuluyor.
+Car car = new Car();
+car.Brand = "Toyota";
+car.Model = "Yaris";
+car.Color = "Beyaz";
+car.NumberDoors = 4;
+car.CarInfo();
+
+
+Console.WriteLine("---------------");
+Car car1 = new Car();
+car1.Brand = "Ford";
+car1.Model = "Fiesta";
+car1.Color = "Kırmızı";
+car1.NumberDoors = 3;
+car1.CarInfo();
+
